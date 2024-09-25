1d119c7ee7ba7339689ea8ae765cb08c1152ca4a
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-09-25 11:50:07 +0530

    Update mongo.py

--- TanuMusic/core/mongo.py
+++ TanuMusic/core/mongo.py
@@ -1,11 +1,4 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/TanuMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/TanuMusic/blob/master/LICENSE >
-#
-# All rights reserved.
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
