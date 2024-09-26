02ad520ae63e7aa5e73194a9ad3e50eba3497c4b
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-09-26 17:00:05 +0530

    Update stream.py

--- TanuMusic/utils/stream/stream.py
+++ TanuMusic/utils/stream/stream.py
@@ -11,7 +11,7 @@ from TanuMusic.misc import db
 from TanuMusic.utils.database import add_active_video_chat, is_active_chat
 from TanuMusic.utils.exceptions import AssistantErr
 from TanuMusic.utils.inline import aq_markup, close_markup, stream_markup
-from TanuMusic.utils.pastebin import AnonyBin
+from TanuMusic.utils.pastebin import TanuBin
 from TanuMusic.utils.stream.queue import put_queue, put_queue_index
 from TanuMusic.utils.thumbnails import get_thumb
 
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await Anony.force_stop_stream(chat_id)
+        await Tanu.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await AnonyBin(msg)
+            link = await TanuBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(
+            await Tanu.join_call(
                 chat_id,
                 original_chat_id,
                 link,
