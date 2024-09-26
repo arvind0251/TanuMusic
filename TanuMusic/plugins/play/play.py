97255a337bc7b1530dd1959d9e806adbdfc6f84a
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-09-26 08:21:22 +0530

    Update play.py

--- TanuMusic/plugins/play/play.py
+++ TanuMusic/plugins/play/play.py
@@ -7,7 +7,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from TanuMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from TanuMusic.core.call import Anony
+from TanuMusic.core.call import Tanu
 from TanuMusic.utils import seconds_to_min, time_to_seconds
 from TanuMusic.utils.channelplay import get_channeplayCB
 from TanuMusic.utils.decorators.language import languageCB
@@ -288,7 +288,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await Anony.stream_call(url)
+                await Tanu.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
