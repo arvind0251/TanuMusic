b59e2aa1b8c3d47ebf0e1f8bc7963ed8e39f0745
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-09-26 19:19:46 +0530

    Update dir.py

--- TanuMusic/core/dir.py
+++ TanuMusic/core/dir.py
@@ -1,10 +1,23 @@
-
 import os
+import logging
+import sys
+from os import listdir, mkdir
 
+from config import TEMP_DB_FOLDER
 from ..logging import LOGGER
 
 
 def dirr():
+    assets_folder = "assets"
+    downloads_folder = "downloads"
+    cache_folder = "cache"
+
+    if assets_folder not in listdir():
+        logging.warning(
+            f"{assets_folder} Folder not Found. Please clone or fork repository again."
+        )
+        sys.exit()
+        
     for file in os.listdir():
         if file.endswith(".jpg"):
             os.remove(file)
@@ -12,10 +25,19 @@ def dirr():
             os.remove(file)
         elif file.endswith(".png"):
             os.remove(file)
-
+        elif file.endswith(".mp3"):
+            os.remove(file)
+        elif file.endswith(".session"):
+            os.remove(file)
+        elif file.endswith(".session-journal"):
+            os.remove(file)
+            
     if "downloads" not in os.listdir():
         os.mkdir("downloads")
     if "cache" not in os.listdir():
         os.mkdir("cache")
+    if "TEMP_DB_FOLDER" not in os.listdir():
+        os.mkdir("TEMP_DB_FOLDER")
+    
 
     LOGGER(__name__).info("❖ Directories Updated...🧡")
