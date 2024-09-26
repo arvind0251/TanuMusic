7576395ddd68ced2341d20eb5562a4951a226cea
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-09-26 08:27:31 +0530

    Update watcher.py

--- TanuMusic/plugins/misc/watcher.py
+++ TanuMusic/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from TanuMusic import app
-from TanuMusic.core.call import Anony
+from TanuMusic.core.call import Tanu 
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Anony.stop_stream_force(message.chat.id)
+    await Tanu.stop_stream_force(message.chat.id)
